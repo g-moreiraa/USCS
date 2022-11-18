6a89a19d3ddedf391fdeea0567fe26fc752de89a
Guilherme Moreira <117770329+g-moreiraa@users.noreply.github.com>
2022-11-18 00:49:19 -0300

    Add files via upload

---
+++ Algoritmo/String/Main.java
@@ -0,0 +1,13 @@
+public class Main
+{
+ public static void main(String[] args){
+   
+   String s = "USCS";
+   int a = s.length();
+   
+   System.out.println(a);
+   System.out.println(s.charAt(0));
+   System.out.println(s.charAt(a-1));
+   
+  }
+}
